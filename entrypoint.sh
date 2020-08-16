44565ced8ff1c68ce1f9ac2c2b12acc10f442f54
heksahititw <hekuran.sahiti@talentwunder.com>
2020-08-16 14:18:10 +0200

    removed aws profile

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,28 +4,6 @@ set -ex
 
 AWS_S3_BUCKET=testbox-"$(echo $GITHUB_REF | sed 's:.*/::')"
 
-#if [ -z "$AWS_ACCESS_KEY_ID_TESTBOX" ]; then
-#  echo "AWS_ACCESS_KEY_ID_TESTBOX is not set. Quitting."
-#  exit 1
-#fi
-#
-#if [ -z "$AWS_SECRET_ACCESS_KEY_TESTBOX" ]; then
-#  echo "AWS_SECRET_ACCESS_KEY_TESTBOX is not set. Quitting."
-#  exit 1
-#fi
-#
-## Default to eu-central-1 if AWS_REGION not set.
-#if [ -z "$AWS_REGION" ]; then
-#  AWS_REGION="eu-central-1"
-#fi
-#
-#aws configure --profile s3-action <<-EOF > /dev/null 2>&1
-#${AWS_ACCESS_KEY_ID_TESTBOX}
-#${AWS_SECRET_ACCESS_KEY_TESTBOX}
-#${AWS_REGION}
-#text
-#EOF
-
 # Create S3 bucket
 BUCKET_NAME=$(aws s3 ls | grep ${AWS_S3_BUCKET} | awk '{print $3}')
 
@@ -33,28 +11,18 @@ if [ -z "${BUCKET_NAME}" ];
 then
 
   sh -c "aws s3 mb s3://${AWS_S3_BUCKET} \
-                --profile s3-action \
                 --region ${AWS_REGION}"
   echo "creating the bucket"
 
   sh -c "aws s3 cp ${GITHUB_WORKSPACE} s3://${AWS_S3_BUCKET}/ \
-                --profile s3-action \
                 --recursive"
   echo "Copying files"
 
 else
 
   sh -c "aws s3 sync ${GITHUB_WORKSPACE} s3://${AWS_S3_BUCKET}/ \
-              --profile s3-action \
               --no-progress"
   echo "syncing files"
 
 fi
 
-# Clear out credentials after we're done.
-aws configure --profile s3-action <<-EOF > /dev/null 2>&1
-null
-null
-null
-text
-EOF
